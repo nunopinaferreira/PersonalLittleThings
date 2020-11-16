db7c7aa1053ebb3468ffd1955c5c9008968045cf
Nuno Ferreira <nunopinaferreira@gmail.com>
2020-11-16 22:27:52 +0000

    fiddled around with css

---
+++ SomeToBeDefinedGameThing/creation.js
@@ -0,0 +1,4 @@
+console.log('Creation!');
+
+$('#divTwo').append('<img src="lib/Mario.png" id="Mario" class="fighter">');
+$('#divTwo').append('<img src="lib/Artemis.png" id="artemis" class="fighter">');
--- SomeToBeDefinedGameThing/setup.js
+++ SomeToBeDefinedGameThing/setup.js
@@ -1,13 +1,41 @@
 console.log('testing setup');
 
+
+
+$('#divOne').append('<img id="streetTest" src="lib/street.jpg" width="800" height="550">');
+console.log('1st one');
+
+const canvas = document.getElementById('fightBox');
+const fightBoxContext = canvas.getContext('2d');
+
+
+/*
+const mario = new Image();
+mario.src = 'lib/mario-grid.png';
+mario.onload = () => {
+    fightBoxContext.drawImage(mario, 0, 0, 50, 50);
+};
+*/
+
+fightBoxContext.beginPath();
+fightBoxContext.rect(20, 40, 20, 20);
+fightBoxContext.fillStyle = 'purple';
+fightBoxContext.fill();
+fightBoxContext.closePath(); 
+
+
+
 // testing jQuery
 createButton = function() {
-    $('body').append ('<input type="button" id="gobutton" onclick="flipButton()" value="flip"></input>');
+    $('#divThree').append ('<input type="button" id="gobutton" onclick="flipButton()" value="Battle!"></input>');
 }
 
 createButton();
 
 
+
+
+
 var flipButton = function() {
-    $.getScript("individual.js");
+    $.getScript("creation.js");
 }
